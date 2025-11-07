c1b5a7ae20ae96dbe9a3bca2dc3a45d3165819ab
parvans <parvansajeevan@gmail.com>
2025-11-07 09:42:38 +0530

    added logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,3 +75,11 @@ export const login = async(req, res) => {
         res.status(500).json({ message: 'Internal server error' }); 
     }
 }
+
+export const logout = (_, res) => {
+    res.cookie('jwt', '',{
+        maxAge: 0,
+    });
+
+    res.status(200).json({ message: 'Logged out successfully' });
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,14 +1,10 @@
 import express from 'express';
-import { login, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/login',login);
-
-router.post('/logout', (req, res) => {
-    // Logout logic here
-    res.json({ message: 'User logged out successfully' });
-});
+router.post('/logout',logout);
 
 export default router;
