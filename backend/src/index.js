f05f94bf6ea5f903d890e92b75150272359ae65e
parvans <parvansajeevan@gmail.com>
2025-11-16 19:23:38 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,12 +26,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (_, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-    });
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname, '/frontend/dist')));
+//     app.get('*', (_, res) => {
+//         res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+//     });
+// }
 
 app.get('/', (req, res) => {
     res.json({ message: 'API is running...' });
